559cb4d477c4b48d269a6de575b5e9c0a3e62221
tanphuccgl <phamnguyentanphucgl@gmail.com>
2022-01-24 10:45:56 +0700

    test

--- bin/dart_challenge.dart
+++ bin/dart_challenge.dart
@@ -1,3 +1,19 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+import 'dart:io';
+
+void main() {
+  print("input");
+  int number;
+  List<int> list = [];
+  do {
+    number = int.parse(stdin.readLineSync().toString());
+    if (number <= 0) {
+      print("input");
+    }
+  } while (number <= 0);
+  for (int i = 1; i <= number; i++) {
+    if (number % i == 0) {
+      list.add(i);
+    }
+  }
+  print(list);
 }
