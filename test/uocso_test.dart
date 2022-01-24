47613850a57c3e34897ea6acf76f5c8159760f8a
j1mmyto9 <khanhuitse05@gmail.com>
2022-01-24 11:12:47 +0700

    fix Unit test

--- test/uocso_test.dart
+++ test/uocso_test.dart
@@ -4,7 +4,7 @@ import '../bin/uocso.dart';
 void main() {
   group('Unit Test 1 - Ước số', () {
     test('Input = 10', () {
-      expect(uocSoOf(5), [1, 2, 5, 10]);
+      expect(uocSoOf(10), [1, 2, 5, 10]);
     });
 
     test('Input = 5', () {
