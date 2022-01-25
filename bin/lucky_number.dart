29081dc256746cc2891a78ad86a3971568280b63
j1mmyto9 <khanhuitse05@gmail.com>
2022-01-25 15:00:06 +0700

    review

--- bin/lucky_number.dart
+++ bin/lucky_number.dart
@@ -5,26 +5,30 @@
 import 'dart:io';
 import 'dart:math';
 
+// radom
+// ___
+// nhap
+// if = win
+// thong bao
+// ---
 void playGame() {
   var rng = Random();
   int score = 100;
-  int number;
   int luckyNumber = (rng.nextInt(100) + 1);
-  print("Đoán số: ");
-  number = int.parse(stdin.readLineSync().toString());
+  print(
+      'Random số từ 1 đến 100, sau đó hỏi user đoán. và gợi ý user nhỏ hơn, lớn hơn, hay bằng');
   while (true) {
-    if (number > luckyNumber) {
-      score--;
-      print("Số nhỏ hơn $number: ");
-      number = int.parse(stdin.readLineSync().toString());
-    } else if (number < luckyNumber) {
-      score--;
-      print("Số lớn hơn $number: ");
-      number = int.parse(stdin.readLineSync().toString());
-    } else {
+    print("Đoán số: ");
+    var number = int.parse(stdin.readLineSync().toString());
+    //
+    if (number == luckyNumber) {
       print("Số bạn chọn bằng với số may mắn: $luckyNumber");
       print("Điểm đạt được: $score\n\n");
       break;
+    } else {
+      score--;
+      print("Số ${number < luckyNumber ? 'nhỏ' : 'lớn'} hơn $number: ");
+      number = int.parse(stdin.readLineSync().toString());
     }
   }
 }
